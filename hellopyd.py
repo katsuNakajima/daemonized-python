6caa4e401114e0d656da8edfbde456a08d862f5c
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2019-03-06 13:27:46 +0900

    fix format

--- hellopyd.py
+++ hellopyd.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import time
+
+
 def main():
     filepath = "/tmp/hello.log"
-    log = open(filepath,'a')
+    log = open(filepath, 'a')
     log.write("hello!\n")
+
 
 if __name__ == '__main__':
     while True:
